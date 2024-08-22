cb2d4ec103d3b49b1a8ce8f7f35fa0e285f42752
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-08-23 04:46:31 +0600

    Refactoring

--- BLL/Services/Interfaces/IUserService.cs
+++ BLL/Services/Interfaces/IUserService.cs
@@ -4,9 +4,9 @@ namespace BLL.Services.Interfaces;
 
 public interface IUserService
 {
-        Task<User?> GetByIdAsync(long id);
-        Task<List<User>> GetAllAsync();
-        Task<long> AddAsync(User user);
-        Task<long> UpdateAsync(User user);
-        Task<bool> DeleteAsync(long id);
+    Task<User?> GetByIdAsync(long id);
+    Task<List<User>> GetAllAsync();
+    Task<long> AddAsync(User user);
+    Task<long> UpdateAsync(User user);
+    Task<bool> DeleteAsync(long id);
 }
