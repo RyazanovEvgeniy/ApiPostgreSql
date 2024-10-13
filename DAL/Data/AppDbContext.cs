b0c88310f90ee37d3e6cdbd9fab512661b69f258
Ryazanov Evgeniy <ryazanov_eugeniy@mail.ru>
2024-10-13 22:39:32 +0600

    Refactoring

--- DAL/Data/AppDbContext.cs
+++ DAL/Data/AppDbContext.cs
@@ -8,10 +8,7 @@ public class AppDbContext : DbContext
 {
     public DbSet<User> Users { get; set; } = null!;
 
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-        Database.EnsureCreated();
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) => Database.Migrate();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
